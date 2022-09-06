3b84e11e2ce9298f36760f1e64212b6ad84eb947
Aleksey Shaydullin <production@blackbirdradio.ru>
2022-09-06 10:58:22 +0300

    Сделать заготовку проекта

---
+++ src/components/AppHeader/AppHeader.jsx
@@ -0,0 +1,12 @@
+import style from './AppHeader.module.css';
+import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
+
+function AppHeader() {
+  return (
+    <header className={style.header}>
+
+    </header>
+  )
+}
+
+export default AppHeader;
