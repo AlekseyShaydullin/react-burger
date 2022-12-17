2ce610e776bf7cdccc47970b83c7aac1859e0494
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2022-12-17 19:44:39 +0300

    feat: layout OrderFeedPage

--- src/pages/OrderFeedPage/OrderFeedPage.jsx
+++ src/pages/OrderFeedPage/OrderFeedPage.jsx
@@ -11,7 +11,37 @@ function OrderFeedPage() {
           <Order />
         </ul>
       </section>
-      <section className={styleOrderFeedPage.statistics}></section>
+      <section className={styleOrderFeedPage.statistics}>
+        <div className={styleOrderFeedPage.ordersNumber}>
+          <div className={styleOrderFeedPage.ordersWrapper}>
+            <h2 className={styleOrderFeedPage.title}>Готовы:</h2>
+            <ul className={styleOrderFeedPage.list}>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034533</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034538</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034533</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034538</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034533</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item} ${styleOrderFeedPage.item_job}`}>034538</li>
+            </ul>
+          </div>
+          <div className={styleOrderFeedPage.ordersWrapper}>
+            <h2 className={styleOrderFeedPage.title}>В работе:</h2>
+            <ul className={styleOrderFeedPage.list}>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item}`}>034538</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item}`}>034538</li>
+              <li className={`text text_type_digits-default ${styleOrderFeedPage.item}`}>034538</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styleOrderFeedPage.allStatistics}>
+          <h2 className={styleOrderFeedPage.title}>Выполнено за все время:</h2>
+          <p className={`text text_type_digits-large ${styleOrderFeedPage.text}`}>28 752</p>
+        </div>
+        <div className={styleOrderFeedPage.allStatistics}>
+          <h2 className={styleOrderFeedPage.title}>Выполнено за сегодня:</h2>
+          <p className={`text text_type_digits-large ${styleOrderFeedPage.text}`}>138</p>
+        </div>
+      </section>
     </main>
   )
 }
